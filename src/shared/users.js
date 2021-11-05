61a3e4ea2316224b033c6534f988c340546bee92
Brian Jun <bmtef.02@gmail.com>
2021-11-04 23:50:54 -0700

    11.4 BJ - Add Shared Folder

--- src/shared/users.js
+++ src/shared/users.js
@@ -1,10 +1,38 @@
-import * as ActionTypes from './ActionTypes';
-
-export const Users = (state = { isLoading: true, errMess: null, users: [] }, action) => {
-    switch (action.type) {
-        case ActionTypes.USERS_FAILED:
-            return {...state, isLoading: false, errMess: action.payload};
-        default:
-            return state;
-    }
-}
+export const USERS = [
+    {
+        id: 0,
+        name: 'Mike Chen',
+        username: 'mikerchen',
+        user_img: '/assets/images/mike.jpg'
+    },
+    {
+        id: 1,
+        name: 'Brian Jun',
+        username: 'bjun',
+        user_img: '/assets/images/brian.png'
+    },
+    {
+        id: 2,
+        name: 'Sylvia Seo',
+        username: 'sseo',
+        user_img: '/assets/images/brian.png'
+    },
+    {
+        id: 3,
+        name: 'Bryant Lee',
+        username: 'blee',
+        user_img: '/assets/images/brian.png'
+    },
+    {
+        id: 4,
+        name: 'Andrew Huynh',
+        username: 'ahuynh',
+        user_img: '/assets/images/brian.png'
+    },
+    {
+        id: 5,
+        name: 'Mel Jimenez',
+        username: 'ahuynh',
+        user_img: '/assets/images/brian.png'
+    },
+]
