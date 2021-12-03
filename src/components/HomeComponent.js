8683d27d6c58acdf886cb9eceadca5fda065a9a1
Michael Chen <chen.michaelr@gmail.com>
2021-12-02 21:02:19 -0800

    new home start 12.2.21 MC

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
-import { Jumbotron,
-    Container, Row, Col  } from 'reactstrap';
+import { Jumbotron, Container, Row, Col  } from 'reactstrap';
 import SearchBar from './SearchBarComponent';
 import SearchFilter from './SearchFilterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { Loading } from '../components/LoadingComponent'
     
-
 class Home extends Component {
 
     render() {
         return(
             <React.Fragment>
-                <Jumbotron fluid className="mb-5">
+                <Jumbotron className="home-jumbotron mb-5">
                     <div className="container">
                         <div className="row">
                             <div className="col">
-                                <h1>Programmers Unite</h1>
-                                <h2>Team Up.</h2>
+                                <h1>Ready to Collaborate?</h1>
+                                <h2>Good.</h2>
+                            </div>
+                            <div classNam="col">
+                                    <button>Get Started</button>
                             </div>
                         </div>
                     </div>
                 </Jumbotron>
-                <div className="center">
-                    <SearchBar colSpan="10" />
-                    {/* <SearchFilter /> */}
-                </div>
             </React.Fragment>
         )
     }
