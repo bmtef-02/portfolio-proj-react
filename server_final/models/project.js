e1e37f37fe8e7ed46ba3d2faea7e9150070ecd1c
Brian Jun <bmtef.02@gmail.com>
2022-01-16 22:21:55 -0800

    projectSchema, projectRouter w/ GET, POST, PUT

---
+++ server_final/models/project.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: String
+    },
+    languages: {
+        type: [String],
+        require: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    teamSize: {
+        type: Number,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    teamIDs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    timestamps: true
+});
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
---
+++ server_final/routes/projectRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const Project = require('../models/project');
+
+const projectRouter = express.Router();
+
+projectRoutuer.route('/')
+.get((req, res, next) => {  // Get all projects
+    Project.find()
+    .populate('owner')
+    .populate('teamIDs')
+    .then(projects => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(projects);
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {     // Create a new project
+    Project.create(req.body)
+    .then(project => {
+        console.log('Project Create ', project);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(project);
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {        // PUT not supported
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /projects')
+})
+
+/* 
+TO DO:
+-make authenticate.js
+    -update user.js with passport-local-mongoose
+    -configure Passport, Passport-Local, JWT
+    -make verifyUser function
+    -make verifyAdmin function
+*/
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {     // deletes all documents in project collection
+    Project.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+})
